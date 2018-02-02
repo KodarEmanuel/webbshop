a585a2d465cfa264afb38a5277c256eff7337b94
emanuel johansson <emanuel_joh@hotmail.com>
2018-02-02 20:19:53 +0100

    Inital Commit

---
+++ Webshop/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webshop.Models
+{
+    public class Product
+    {
+      public int Id;
+      public string Name;
+      public string Description;
+      public int Price;
+      public string[] PicturePaths;
+    }
+}
