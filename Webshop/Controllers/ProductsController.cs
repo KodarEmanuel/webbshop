5c6f657e0e19199dec5df7eff85ed59e515c6794
emanuel johansson <emanuel_joh@hotmail.com>
2018-02-04 18:58:04 +0100

    a

---
+++ Webshop/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webshop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult Category(string _PageNum)
+        {
+            return View("~/Views/Products/Page1.cshtml");
+        }
+       
+
+    }
+}
+
+
---
+++ Webshop/Models/Data/DataClas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webshop.Models.Data
+{
+    public class DataClas
+    {
+        private string Name = "Catagory";
+
+        public string Catagory
+        {
+            get
+            {
+                return "1";
+            }
+        }
+    }
+}
